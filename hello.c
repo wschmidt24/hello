86ed0209b48da37ddb6c9a5be9b8813b2f9ea7a4
Andrew Foster <andrewfoster@andrews-mbp-7.unl.edu>
2022-08-26 16:30:21 -0500

    Added my name and major too.

--- hello.c
+++ hello.c
@@ -9,7 +9,8 @@
  int main(int argc, char **argv) {
 
     printf("Hello, I am Will Schmidt\n");
-    printf("I'm a computer science major\n");
+    printf("Hello, My name is Andy Foster!\n");
+    printf("We are computer science majors\n");
     printf("I do not have a minor\n");
 
     return 0;
