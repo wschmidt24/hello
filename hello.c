c20c39f543756404446c7a6b82207dec107981ff
Will Schmidt <wschmidt3@huskers.unl.edu>
2022-08-26 15:57:51 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/*
+ * Basic Hello World Program
+ * */
+
+ int main(int argc, char **argv) {
+
+    printf("Hello, I'm Will Schmidt\n")
+    printf("I'm a computer science major\n")
+
+    return 0;
+ }
