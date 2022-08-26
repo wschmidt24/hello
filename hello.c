a42f81b5eedf5342a857367f67e555eae7e46880
Will Schmidt <wschmidt3@huskers.unl.edu>
2022-08-26 21:09:54 +0000

    added my minor

--- hello.c
+++ hello.c
@@ -1,11 +1,16 @@
-/*
+/**
  * Basic Hello World Program
  * */
 
+#include <stdlib.h>
+#include <stdio.h>
+
+
  int main(int argc, char **argv) {
 
-    printf("Hello, I am Will Schmidt\n")
-    printf("I'm a computer science major\n")
+    printf("Hello, I am Will Schmidt\n");
+    printf("I'm a computer science major\n");
+    printf("I do not have a minor\n");
 
     return 0;
  }
